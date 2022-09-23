77db6e9c69d4cfadd876a50830d052bc26ebd2fc
freshmen <freshmen@damien-hs.edu>
2022-09-23 12:02:15 -0700

    finished

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,23 @@ public class Main {
     public static void main(String[] args) {
         MyLinkedList<Integer> list = new MyLinkedList<>();
         System.out.println(list);
-
-        list.addFirst(5);
-        list.addFirst(53);
-        list.addFirst(4);
-        list.addFirst(2);
-        list.addLast(3);
-        list.addLast(6);
+        try{
+            list.addFirst(5);
+            list.addFirst(53);
+            list.addFirst(4);
+            list.addFirst(2);
+            list.addLast(3);
+            list.addLast(6);
+//            list.removeFirst();
+//            list.removeLast();
+//            list.removeIndex(2);
+            list.addIndex(5,9);
+        }catch (NullPointerException e){
+            System.out.println("RECOVERED: attempted to remove an item from an empty list");
+        }
+        catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("RECOVERED: attempted to remove an item from out of the list");
+        }
         System.out.println(list);
     }
 }
--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -16,6 +16,7 @@ public class MyLinkedList <T>{
             last.setNext(temp);
             last = temp;
         }
+        size++;
     }
     public void addFirst(T data){
         MyListNode<T> temp = new MyListNode<>(data);
@@ -31,9 +32,71 @@ public class MyLinkedList <T>{
         }
         size ++;
     }
-//    public T removeFirst(){
-//
-//    }
+    public T removeFirst(){
+        if(first ==null){
+            throw new NullPointerException("MyLinkedList is empty");
+        }
+        MyListNode<T> temp = first;
+        first = first.getNext();
+        temp.setNext(null);
+        return null;
+    }
+    public T removeIndex(int n){
+        MyListNode<T>  IndexNum= first;
+        MyListNode<T> prevNumOnIndex = IndexNum;
+        int count = 0;
+        if(n ==0){
+            removeFirst();
+            return null;
+        }
+//        try{
+//            while(count != n){
+//                prevNumOnIndex = IndexNum;
+//                IndexNum = prevNumOnIndex.getNext();
+//                count++;
+//            }
+//        }catch (ArrayIndexOutOfBoundsException e){
+//            throw new ArrayIndexOutOfBoundsException("Index out of bound");
+//        }
+        while(count != n){
+            prevNumOnIndex = IndexNum;
+            IndexNum = prevNumOnIndex.getNext();
+            count++;
+        }
+        prevNumOnIndex.setNext(IndexNum.getNext());
+        IndexNum.setNext(null);
+        return null;
+    }
+    public T addIndex(int n,T data){
+        MyListNode<T>  IndexNum= first;
+        MyListNode<T> prevNumOnIndex = IndexNum;
+        MyListNode<T> temp = new MyListNode<>(data);
+        int count = 0;
+        while(count != n){
+            prevNumOnIndex = IndexNum;
+            IndexNum = prevNumOnIndex.getNext();
+            count++;
+        }
+        prevNumOnIndex.setNext(temp);
+        temp.setNext(IndexNum);
+        return null;
+    }
+    public T removeLast(){
+        if(first ==null){
+            throw new NullPointerException("MyLinkedList is empty");
+        }
+        if(first == last){
+            first = null;
+            last = null;
+        }
+        MyListNode<T> previous = first;
+        while(previous.getNext() != last){
+            previous=previous.getNext();
+        }
+        last = previous;
+        last.setNext(null);
+        return null;
+    }
     public  int size(){
         return size;
     }
@@ -44,7 +107,6 @@ public class MyLinkedList <T>{
         while(current != null){
             out += current.getData() + "->";
             current = current.getNext();
-            System.out.println(out);
         }
         out += "null";
         return out;
